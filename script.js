dad3de42478b9415dc36b93bd3234cf1f6dd9676
sundaraselvam <vickiesundar2000@gmail.com>
2025-10-11 21:06:41 +0530

    added randomizing and handle click's plan

--- script.js
+++ script.js
@@ -11,8 +11,21 @@ const cardAssignments = () => {
             if (!duCards.includes(cardValues)) {
                 duCards.push(cardValues);
             }
+            else {
+                i--;
+            }
         }
     }
     allCards.push(...ogCards, ...duCards);
-    return allCards;
+    for(let j = 1; j <= 16; j++){
+        const imageElement = document.getElementById(j);
+        imageElement.src = `assets/${allCards[j-1]}.png`;
+    }
 };
+const handleClick = (id) => {
+    // 1. Got to create a variable that will check the previous card clicked
+    // 2. if the current card clicked matches the previous card clicked, then we leave both cards face up
+    // 3. if the current card clicked does not match the previous card clicked, then we flip both cards back over
+    // 4. We also need to make sure that if a card is already face up, we do not do anything when it is clicked again
+    return id;
+}
