8ad278b0582243bb454f5ffd1f65381dab72c769
sundaraselvam <vickiesundar2000@gmail.com>
2025-10-11 20:48:21 +0530

    card assignment

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const cardAssignments = () => {
-    const cards = [];
-    const cardValues = Math.floor(Math.random() * 8 + 1);
-    if(!cards.includes(cardValues)){
-        cards.push(cardValues);
+    const ogCards = [];
+    const duCards = [];
+    const allCards = [];
+    for (let i = 1; i <= 16; i++) {
+        const cardValues = Math.floor(Math.random() * 8 + 1);
+        if (!ogCards.includes(cardValues)) {
+            ogCards.push(cardValues);
+        }
+        else {
+            if (!duCards.includes(cardValues)) {
+                duCards.push(cardValues);
+            }
+        }
     }
-    else{
-        cardAssignments();
-    }
+    allCards.push(...ogCards, ...duCards);
+    return allCards;
+    
 };
