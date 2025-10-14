f36f130b0ff53a697bd6505c68680ca01fb17f0d
sundaraselvam <vickiesundar2000@gmail.com>
2025-10-15 01:31:46 +0530

    added proper code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function loading(){
-    cards = [{id:1, img: "assets/1.png"}, {id:2, img: "assets/2.png"}, {id:1, img: "assets/1.png"}, {id:1, img: "assets/1.png"},{id:1, img: "assets/1.png"},{id:1, img: "assets/1.png"},{id:1, img: "assets/1.png"},{id:1, img: "assets/1.png"} {id:1, img: "assets/1.png"}, {id:2, img: "assets/2.png"}];
-    console.log(shuffle(cards));
+    cards = [{id:1, img: "assets/1.png"}, {id:2, img: "assets/2.png"}, {id:3, img: "assets/3.png"}, {id:4, img: "assets/4.png"},{id:5, img: "assets/5.png"},{id:6, img: "assets/6.png"},{id:7, img: "assets/7.png"},{id:8, img: "assets/8.png"} ,{id:1, img: "assets/1.png"}, {id:2, img: "assets/2.png"}, {id:3, img: "assets/3.png"}, {id:4, img: "assets/4.png"},{id:5, img: "assets/5.png"},{id:6, img: "assets/6.png"},{id:7, img: "assets/7.png"},{id:8, img: "assets/8.png"}];
+    assignableCards = shuffle(cards);
+    assignCards(assignableCards);
 }
 function shuffle(cards){
     let currentIndex = cards.length;
@@ -15,6 +16,12 @@ function shuffle(cards){
     }
     return cards;
 };
+function assignCards(assignableCards){
+    for (let i = 1; i <= 16; i ++){
+        const imageElement = document.getElementById(i);
+        imageElement.src = assignableCards[i-1].img;
+    }
+}
 
 // card []
 // back
