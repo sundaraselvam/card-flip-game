6357d744de2e9ebf5c7079d6efcab52b75da7861
sundaraselvam <vickiesundar2000@gmail.com>
2025-10-15 02:41:15 +0530

    flip index and other stuff added

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let flipIndex = [];
+let flippedCards = [];
+let matchedCards = [];
+let lockBoard = false;
 function loading(){
     cards = [{id:1, img: "assets/1.png"}, {id:2, img: "assets/2.png"}, {id:3, img: "assets/3.png"}, {id:4, img: "assets/4.png"},{id:5, img: "assets/5.png"},{id:6, img: "assets/6.png"},{id:7, img: "assets/7.png"},{id:8, img: "assets/8.png"} ,{id:1, img: "assets/1.png"}, {id:2, img: "assets/2.png"}, {id:3, img: "assets/3.png"}, {id:4, img: "assets/4.png"},{id:5, img: "assets/5.png"},{id:6, img: "assets/6.png"},{id:7, img: "assets/7.png"},{id:8, img: "assets/8.png"}];
     assignableCards = shuffle(cards);
@@ -19,11 +23,12 @@ function shuffle(cards){
 function assignCards(assignableCards){
     for (let i = 1; i <= 16; i ++){
         const imageElement = document.getElementById(i);
-        imageElement.src = assignableCards[i-1].img;
+        imageElement.src = 'assets/cover.png';
+        flipIndex[i-1] = assignableCards[i-1].img;
     }
-}
-
-// card []
-// back
-// front
-// position absolute/relative
+};
+function handleClick(id){
+    const imageElement = document.getElementById(id);
+    imageElement.classList.add("flip");
+    imageElement.src = flipIndex[id-1];
+};
